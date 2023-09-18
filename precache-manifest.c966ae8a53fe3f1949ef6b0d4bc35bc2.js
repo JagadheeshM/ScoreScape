fa46704c98e187fd4828e8ba8586649d828cd12d
maheshwaramjagadheesh@gmail.com <maheshwaramjagadheesh@gmail.com>
2023-09-18 18:40:18 +0530

    Updates

---
+++ precache-manifest.c966ae8a53fe3f1949ef6b0d4bc35bc2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "02a043977db67113b327",
+    "url": "/ScoreScape/static/css/main.f64c7ae4.chunk.css"
+  },
+  {
+    "revision": "02a043977db67113b327",
+    "url": "/ScoreScape/static/js/main.02a04397.chunk.js"
+  },
+  {
+    "revision": "766be002cfdbb69832ec",
+    "url": "/ScoreScape/static/js/1.766be002.chunk.js"
+  },
+  {
+    "revision": "b2a926c0856477e9c32f",
+    "url": "/ScoreScape/static/css/2.91403d14.chunk.css"
+  },
+  {
+    "revision": "b2a926c0856477e9c32f",
+    "url": "/ScoreScape/static/js/2.b2a926c0.chunk.js"
+  },
+  {
+    "revision": "bc7c8db457b0f6f40135",
+    "url": "/ScoreScape/static/js/runtime~main.bc7c8db4.js"
+  },
+  {
+    "revision": "5369a825930fbab7ba23d6b1a3c0cdd9",
+    "url": "/ScoreScape/index.html"
+  }
+];
